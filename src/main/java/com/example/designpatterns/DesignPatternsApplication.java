b9f1e6a77de178b0ea8b143c0557dcf50b7a7fb0
pragnyananda <45530046+pragnyananda@users.noreply.github.com>
2023-05-07 20:30:59 +0530

    first commit

---
+++ src/main/java/com/example/designpatterns/DesignPatternsApplication.java
@@ -0,0 +1,20 @@
+package com.example.designpatterns;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class DesignPatternsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args)  {
+		log.info("running Design Pattern Application.");
+	}
+}
